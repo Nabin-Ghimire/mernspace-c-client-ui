8caefabc9e44a46ff5fe9a255629a3c0bf9c73b3
Nabin Ghimire <ghimiren2057@gmail.com>
2025-08-16 10:46:18 +0545

    shadcn setup

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",   // if you're using Next.js App Router
+    "./pages/**/*.{js,ts,jsx,tsx}",     // if you still have Pages Router
+    "./components/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#4f46e5", // example brand color
+          light: "#818cf8",
+          dark: "#3730a3",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], // override default font
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
