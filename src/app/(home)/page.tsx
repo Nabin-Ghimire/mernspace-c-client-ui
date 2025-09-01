5d7dce4189d95dfe311a30139b1fce885974d83c
Nabin Ghimire <ghimiren2057@gmail.com>
2025-09-01 10:42:03 +0545

    add category tabs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,40 @@
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
+      <section className="bg-white">
+        <div className="container flex items-center justify-between">
+          <div>
+            <h1 className="text-7xl font-black py-24 font-sans leading-2">
+              Super Delicious Pizza in <br /> <br /><br /><br /> <br /><br /><br /><br /> <br />
+              <span className="text-primary">Only 45 Minutes</span>
+            </h1>
+            <p className="text-2xl mt-8 max-w-lg leading-snug">
+              Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
+            </p>
+            <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">Get your pizza now</Button>
+          </div>
+          <div>
+            <Image alt='pizza-main' src={'/pizza.png'} width={400} height={400} />
+          </div>
+        </div>
+      </section>
 
-    <main >
-      <h1>Welcome to pizza shop</h1>
-      <Button variant={'link'}>Button</Button>
-      <Button variant={'ghost'}>Button 2</Button>
-    </main>
-
+      <section>
+        <div className="container py-12">
+          <Tabs defaultValue="pizza" className="w-[400px]">
+            <TabsList>
+              <TabsTrigger value="pizza" className="text-md">Pizza</TabsTrigger>
+              <TabsTrigger value="beverages" className="text-md">Beverages</TabsTrigger>
+            </TabsList>
+            <TabsContent value="pizza">Pizza list</TabsContent>
+            <TabsContent value="beverages">Beverages list</TabsContent>
+          </Tabs>
+        </div>
+      </section>
+    </>
   );
 }
