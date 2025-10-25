1523ce82880ecf81dc517db3e068bca905ac1b36
Nabin Ghimire <ghimiren2057@gmail.com>
2025-10-25 11:48:55 +0545

    fetch-categories for Client

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 import ProductCard, { Product } from "./components/product-card";
+import { Category } from "@/lib/types";
 
 const products: Product[] = [
   {
@@ -55,7 +56,18 @@ const products: Product[] = [
 ]
 
 
-export default function Home() {
+export default async function Home() {
+
+  const categoryResponse = await fetch(`${process.env.BACKEND_URL}/api/catalog/categories`, {
+    next: {
+      revalidate: 3600, //1 hour
+    }
+  })
+
+  if (!categoryResponse.ok) throw new Error('Failed to fetch categories')
+
+  const cotegories: Category[] = await categoryResponse.json();
+  console.log(cotegories)
   return (
     <>
       <section className="bg-white">
@@ -80,8 +92,13 @@ export default function Home() {
         <div className="container py-12">
           <Tabs defaultValue="pizza">
             <TabsList>
-              <TabsTrigger value="pizza" className="text-md">Pizza</TabsTrigger>
-              <TabsTrigger value="beverages" className="text-md">Beverages</TabsTrigger>
+
+              {
+                cotegories.map((category) => { return (<TabsTrigger key={category._id} value={category._id} className="text-md">{category.name}</TabsTrigger>) }
+                )
+              }
+
+              {/* <TabsTrigger value="beverages" className="text-md">Beverages</TabsTrigger> */}
             </TabsList>
             <TabsContent value="pizza">
               <div className="grid grid-cols-4 gap-6 mt-6">
