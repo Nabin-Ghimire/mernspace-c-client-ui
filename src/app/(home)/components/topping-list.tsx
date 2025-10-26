657ad1c393f89676e3afe74cd526ef82f0fc0f93
Nabin Ghimire <ghimiren2057@gmail.com>
2025-10-26 12:17:41 +0545

    rendering toppings in dialog box

--- src/app/(home)/components/topping-list.tsx
+++ src/app/(home)/components/topping-list.tsx
@@ -1,27 +1,43 @@
 'use client'
-import React from 'react'
-import ToppingCard, { Topping } from './topping-card'
+import React, { useEffect, useState } from 'react'
+import ToppingCard from './topping-card'
+import { Topping } from '@/lib/types';
 
-const toppings = [
-  {
-    id: '1', name: 'Chicken', image: '/chicke.jpg', price: 100, isAvailable: true
-  },
-  {
-    id: '2', name: 'Jelapeno', image: '/jalt.jpg', price: 75, isAvailable: true
-  },
-  {
-    id: '3', name: 'Cheese', image: '/cheese.jpg', price: 50, isAvailable: true
-  }
-]
+// const toppings = [
+//   {
+//     id: '1', name: 'Chicken', image: '/chicke.jpg', price: 100, isAvailable: true
+//   },
+//   {
+//     id: '2', name: 'Jelapeno', image: '/jalt.jpg', price: 75, isAvailable: true
+//   },
+//   {
+//     id: '3', name: 'Cheese', image: '/cheese.jpg', price: 50, isAvailable: true
+//   }
+// ]
 
 const ToppingList = () => {
 
-  const [selectedToppings, setSelectedToppings] = React.useState([toppings[0]])
+
+  const [toppings, setToppings] = useState<Topping[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const toppingResponse = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/catalog/toppings?5`);
+
+      const toppingsFetched = await toppingResponse.json();
+      const toppings = toppingsFetched.docs
+      setToppings(toppings);
+      console.log(toppings)
+    };
+    fetchData();
+  }, [])
+
+  const [selectedToppings, setSelectedToppings] = React.useState<Topping[]>([])
 
   const handleCheckBoxCheck = (topping: Topping) => {
-    const isAlreadyExists = selectedToppings.some((element) => element.id === topping.id);
+    const isAlreadyExists = selectedToppings.some((element: Topping) => element.id === topping.id);
     if (isAlreadyExists) {
-      setSelectedToppings((prev) => prev.filter((elem) => elem.id !== topping.id))
+      setSelectedToppings((prev) => prev.filter((elem: Topping) => elem.id !== topping.id))
       return;
     }
     setSelectedToppings((prev) => [...prev, topping]);
@@ -34,7 +50,7 @@ const ToppingList = () => {
         toppings.map((topping) => {
           return (<ToppingCard
             topping={topping}
-            key={topping.id}
+            key={topping.name}
             selectedToppings={selectedToppings}
             handleCheckBoxCheck={handleCheckBoxCheck}
           />
