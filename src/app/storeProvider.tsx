dd838e6f40e53d66c652c2ac2577d47a36c940cd
Nabin Ghimire <ghimiren2057@gmail.com>
2025-11-05 13:18:10 +0545

    persisting store data

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -2,6 +2,7 @@
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore, AppStore } from '../lib/store/store'
+import { setInitialCartItems } from '@/lib/store/features/cart/cartSlice'
 
 export default function StoreProvider({
   children,
@@ -13,7 +14,16 @@ export default function StoreProvider({
     // Create the store instance the first time this renders
     storeRef.current = makeStore()
   }
-  //todo: set initial cart state from local store
+  const isLocatStorageAvailable = typeof window !== 'undefined' && window.localStorage;
+  if (isLocatStorageAvailable) {
+    const cartItems = window.localStorage.getItem('cartItems');
+    try {
+      const parsedItems = JSON.parse(cartItems as string);
+      storeRef.current.dispatch(setInitialCartItems(parsedItems));
 
+    } catch (err) {
+      console.error('Failed to parse cart items from localStorage', err);
+    }
+  }
   return <Provider store={storeRef.current}>{children}</Provider>
 }
